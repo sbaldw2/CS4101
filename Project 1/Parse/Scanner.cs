e008f926d708ee13f6c72d46d62a4515b205b7f9
kristenbarrett <kristennbarrett@gmail.com>
2015-10-01 18:09:14 -0500

    Outlined Scanner (1/2)
    TODO: string->buffer, #->int, and identifier->buffer

--- Project 1/Parse/Scanner.cs
+++ Project 1/Parse/Scanner.cs
@@ -1,334 +1,134 @@
-﻿using System;
+// Scanner -- The lexical analyzer for the Scheme printer and interpreter
+
+using System;
+using System.IO;
 using Tokens;
-using Tree;
 
-namespace prog1
+namespace Parse
 {
+    public class Scanner
+    {
+        private TextReader In;
 
-	// Scanner.java -- the implementation of class Scanner
+        // maximum length of strings and identifier
+        private const int BUFSIZE = 1000;
+        private char[] buf = new char[BUFSIZE];
 
+        public Scanner(TextReader i) { In = i; }
+  
+        // TODO: Add any other methods you need
 
-	internal class Scanner
-	{
-	 private PushbackInputStream @in;
-	 private sbyte[] buf;
+        public Token getNextToken()
+        {
+            int ch;
 
-	 public Scanner(System.IO.Stream i)
-	 {
-	  @in = new PushbackInputStream(i);
-	 }
+            try
+            {
+                // It would be more efficient if we'd maintain our own
+                // input buffer and read characters out of that
+                // buffer, but reading individual characters from the
+                // input stream is easier. peek when you do ints/strs. add white space check. 
+                ch = In.Read() // sees next char
+                if (ch == 10 || ch == 32 || ch == 9 || ch == 13 ) //isEmpty()?
+                    return getNextToken(); // yeah dog keep going
+                else if (ch == 59) // hit a ;
+                {
+                     while(ch != 10) // as long as there's no ;
+                     {
+                        ch = In.Read();
+                        if( ch == -1 || ch == 0) // if stream breaks
+                            break; // return error (null)
+                     }
+                     if (ch == 10) // at da end
+                        return getNextToken();
+                }
+   
+                // TODO: skip white space and comments
+                // use ascii characters to check 
 
-	 public virtual Token NextToken
-	 {
-		 get
-		 {
-		  int bite = -1;
+                else if (ch == -1 || ch == 0)
+                    return null;
+        
+                // Special characters
+                else if (ch == '\'')
+                    return new Token(TokenType.QUOTE);
+                else if (ch == '(')
+                    return new Token(TokenType.LPAREN);
+                else if (ch == ')')
+                    return new Token(TokenType.RPAREN);
+                else if (ch == '.')
+                    // We ignore the special identifier `...'.
+                    return new Token(TokenType.DOT);
+                
+                // Boolean constants
+                else if (ch == '#')
+                {
+                    ch = In.Read();
+                    if (ch == 't')
+                        return new Token(TokenType.TRUE);
+                    else if (ch == 'f')
+                        return new Token(TokenType.FALSE);
+                    else if (ch == -1)
+                    {
+                        Console.Error.WriteLine("Unexpected EOF following #");
+                        return null;
+                    }
+                    else
+                    {
+                        Console.Error.WriteLine("Illegal character '" +
+                                                (char)ch + "' following #");
+                        return getNextToken();
+                    }
+                }
+
+                // String constants
+                else if (ch == '"')
+                {
+                    // TODO: scan a string into the buffer variable buf
+                    // keep going until you hit the next ""?
+                    return new StringToken(new String(buf, 0, 0));
+                }
+
     
-		  // It would be more efficient if we'd maintain our own input buffer
-		  // and read characters out of that buffer, but reading individual
-		  // characters from the input stream is easier.
-		  try
-		  {
-		   bite = @in.read();
-		  }
-		  catch (IOException e)
-		  {
-		   Console.Error.WriteLine("We fail: " + e.Message);
-		   // System.err.println("We fail: " + e.getMessage());      // Be sure to uncomment before turning in**************************************************
-		  }
+                // Integer constants
+                else if (ch >= '0' && ch <= '9')
+                {
+                    int i = ch - '0';
+                    // TODO: scan the number and convert it to an integer
+
+                    // make sure that the character following the integer
+                    // is not removed from the input stream
+                    return new IntToken(i);
+                }
+        
+                // Identifiers
+                else if (ch >= 'A' && ch <= 'Z'
+                         // or ch is some other valid first character
+                         // for an identifier
+                         ) {
+                    // TODO: scan an identifier into the buffer
+
+                    // make sure that the character following the integer
+                    // is not removed from the input stream
+
+                    return new IdentToken(new String(buf, 0, 0));
+                }
     
-		  /*
-		  if (bite == 10) { // Temporary solution for console input, remove before turning in project****************************************************************
-		   try {
-		    in.close();
-		   } catch (IOException e) {
-		    // System.err.println("We fail: " + e.getMessage());
-		   }
-		  }
-		  */
-    
-		  // TODO: skip white space, carriage return, line feed, and tab 
-		  // ****DONE****
-    
-		  if ((bite == 32) || (bite == 13) || (bite == 10) || (bite == 9))
-		  {
-		   return NextToken;
-		  }
-    
-		  /*
-		  while ((bite == 32) || (bite == 13) || (bite == 10) || (bite == 9)) {
-		   try {
-		    bite = in.read();
-		   } catch (IOException e) {
-		    System.err.println("We fail: " + e.getMessage());
-		   }
-		  }
-		  */
-    
-		  // TODO: skip comments
-		  // ****DONE****
-		  if (bite == 59)
-		  { // 59 is ASCII semicolon
-		   while (bite != 10)
-		   { // Stop loop at line feed, the end of a comment
-			try
-			{
-			 bite = @in.read();
-			}
-			catch (IOException e)
-			{
-			 Console.Error.WriteLine("We fail: " + e.Message);
-			}
-			if (bite == -1)
-			{ // if end of stream is reached at end of a comment, return null
-			 return null;
-			}
-			if (bite == 10)
-			{ // arrived to line feed, the end of a comment
-			 // At this point, bite contains line feed, so call getNextToken() for the next character of the new line
-			 return NextToken;
-			} // else, the loop continues
-		   }
-		  }
-    
-    
-		  if (bite == -1)
-		  {
-		   return null;
-		  }
-    
-		  char character = (char) bite;
-    
-		  // Special characters
-		  if (character == '\'')
-		  {
-		   return new Token(Token.QUOTE);
-		  }
-		  else if (character == '(')
-		  {
-		   return new Token(Token.LPAREN);
-		  }
-		  else if (character == ')')
-		  {
-		   return new Token(Token.RPAREN);
-		  }
-		  else if (character == '.')
-		  {
-		   // We ignore the special identifier `...'.
-		   return new Token(Token.DOT);
-		  }
-    
-		  // Boolean constants
-		  else if (character == '#')
-		  {
-		   try
-		   {
-			bite = @in.read();
-		   }
-		   catch (IOException e)
-		   {
-			Console.Error.WriteLine("We fail: " + e.Message);
-		   }
-    
-		   if (bite == -1)
-		   {
-			Console.Error.WriteLine("Unexpected EOF following #");
-			return null;
-		   }
-    
-		   character = (char) bite;
-		   if (character == 't')
-		   {
-			return new Token(Token.TRUE);
-		   }
-		   else if (character == 'f')
-		   {
-			return new Token(Token.FALSE);
-		   }
-		   else
-		   {
-			Console.Error.WriteLine("Illegal character '" + (char) character + "' following #");
-			return NextToken;
-		   }
-		  }
-    
-		  // String constants
-		  else if (character == '"')
-		  {
-		   buf = new sbyte[1000]; // re-create the buffer to clear pre-existing bytes
-		   // TODO: scan a string into the buffer variable buf
-		   // ****DONE****
-		   try
-		   {
-			bite = @in.read();
-		   }
-		   catch (IOException e)
-		   {
-			Console.Error.WriteLine("We fail: " + e.Message);
-		   }
-    
-		   if (bite == 34)
-		   { // check for another double quote here, if it is the case, then we have an empty string, create empty string token
-			return new StrToken("");
-		   }
-    
-		   int pointer = 0; // pointer in the buffer array
-		   while (bite != 34)
-		   { // 34 is ascii for \" (double quote)
-			if (bite == -1)
-			{
-			 Console.Error.WriteLine("Unexpected EOF following \"");
-			 return null;
-			}
-    
-			buf[pointer] = (sbyte) bite; // store the bite on the buffer
-    
-			try
-			{ // get the next byte and loop
-			 bite = @in.read();
-			}
-			catch (IOException e)
-			{
-			 Console.Error.WriteLine("We fail: " + e.Message);
-			}
-    
-			pointer = pointer + 1;
-		   }
-    
-		   return new StrToken(StringHelperClass.NewString(buf));
-		   // old code: return new StrToken(buf.toString());
-		   // new String(buf) converts all ascii code in array to string
-		  }
-    
-		  // Integer constants
-		  else if (character >= '0' && character <= '9')
-		  {
-		   // TODO: scan the number and convert it to an integer
-		   // ****DONE****
-    
-		   string number = ""; // Hacky way to read a number, but works
-    
-		   while (bite >= 48 && bite <= 57)
-		   {
-			number += character;
-    
-			try
-			{ // get the next byte
-			 bite = @in.read();
-			}
-			catch (IOException e)
-			{
-			 Console.Error.WriteLine("We fail: " + e.Message);
-			}
-    
-			if (bite == -1)
-			{
-			 Console.Error.WriteLine("Unexpected EOF following an Integer");
-			 return null;
-			}
-    
-			character = (char) bite;
-		   }
-    
-		   // put the character after the integer back into the input
-		   // in->putback(ch);
-		   try
-		   {
-			@in.unread(bite);
-		   }
-		   catch (IOException e)
-		   {
-			Console.Error.WriteLine("We fail: " + e.Message);
-		   }
-    
-		   int i = int.Parse(number);
-    
-		   return new IntToken(i);
-		  }
-    
-		  // Identifiers
-		  else if ((character >= 'A' && character <= 'Z') || (character >= 'a' && character <= 'z') || (character == '!') || (character == '$') || (character == '%') || (character == '&') || (character == '*') || (character == '/') || (character == ':') || (character == '<') || (character == '=') || (character == '>') || (character == '?') || (character == '^') || (character == '_') || (character == '~') || (character == '+') || (character == '-'))
-			// Special Initials
-			// Peculiar Identifier
-		  { // Initial letters, both upper and lower cases
-		   // TODO: scan an identifier into the buffer
-		   // ****DONE****
-    
-		   // Need to check and return a Peculiar Identifier as token before rest of the grammar happens
-		   if ((character == '+') || (character == '-'))
-		   {
-			return new IdentToken("" + character);
-		   }
-    
-		   buf = new sbyte[1000]; // re-create the buffer to clear pre-existing bytes
-    
-		   buf[0] = (sbyte) character;
-    
-		   try
-		   { // get the next byte
-			bite = @in.read();
-		   }
-		   catch (IOException e)
-		   {
-			Console.Error.WriteLine("We fail: " + e.Message);
-		   }
-    
-		   if (bite == -1)
-		   {
-			Console.Error.WriteLine("Unexpected EOF following an Identifier");
-			// return null;
-		   }
-    
-		   character = (char) bite;
-    
-		   int pointer = 1;
-		   while ((character >= 'A' && character <= 'Z') || (character >= 'a' && character <= 'z') || (character == '!') || (character == '$') || (character == '%') || (character == '&') || (character == '*') || (character == '/') || (character == ':') || (character == '<') || (character == '=') || (character == '>') || (character == '?') || (character == '^') || (character == '_') || (character == '~') || (character >= '0' && character <= '9') || (character == '+') || (character == '-') || (character == '.') || (character == '@'))
-			// Special Initials
-			// Digit
-			// Special subsequent
-		   { // Initial letters, both upper and lower cases
-			buf[pointer] = (sbyte) character;
-    
-			try
-			{ // get the next byte
-			 bite = @in.read();
-			}
-			catch (IOException e)
-			{
-			 Console.Error.WriteLine("We fail: " + e.Message);
-			}
-    
-			if (bite == -1)
-			{
-			 Console.Error.WriteLine("Unexpected EOF following an Identifier");
-			 // return null; 
-			}
-    
-			character = (char) bite;
-			pointer = pointer + 1;
-		   }
-		   // put the character after the identifier back into the input
-		   // in->putback(ch);
-		   try
-		   {
-			@in.unread(bite);
-		   }
-		   catch (IOException e)
-		   {
-			Console.Error.WriteLine("We fail: " + e.Message);
-		   }
-    
-		   return new IdentToken(StringHelperClass.NewString(buf));
-		  }
-    
-		  // Illegal character
-		  else
-		  {
-		   Console.Error.WriteLine("Illegal input character '" + (char) character + '\'');
-		   return NextToken;
-		  }
-		 }
-	 }
-	}
+                // Illegal character
+                else
+                {
+                    Console.Error.WriteLine("Illegal input character '"
+                                            + (char)ch + '\'');
+                    return getNextToken();
+                }
+            }
+            catch (IOException e)
+            {
+                Console.Error.WriteLine("IOException: " + e.Message);
+                return null;
+            }
+        }
+    }
 
 }
+
