0ec48c1184db220ff296c7e3ad20399a9b560942
Kristen Barrett <kbarr14@tigers.lsu.edu>
2015-10-10 11:29:05 -0500

    tester fix

--- Project1/SPP.cs
+++ Project1/SPP.cs
@@ -23,39 +23,33 @@ public class SPP
         if (args.Length == 1 && args[0].Equals("-d"))
         {
             // Console.Write("Scheme 4101> ");
+            Parser parser = new Parser(scanner);
+            Node root;
             Token tok = scanner.getNextToken();
             while (tok != null)
             {
                 TokenType tt = tok.getType();
-
-                Console.Write(tt);
-                if (tt == TokenType.INT)
-                    Console.WriteLine(", intVal = " + tok.getIntVal());
-                else if (tt == TokenType.STRING)
-                    Console.WriteLine(", stringVal = " + tok.getStringVal());
-                else if (tt == TokenType.IDENT)
-                    Console.WriteLine(", name = " + tok.getName());
-                else
-                    Console.WriteLine();
+                root = parser.parseExp();
+                while (root != null)
+                {
+                    root.print(0);
+                    root = parser.parseExp();
+                }
+                //Console.Write(tt);
+                //if (tt == TokenType.INT)
+                //  Console.WriteLine(", intVal = " + tok.getIntVal());
+                //else if (tt == TokenType.STRING)
+                //  Console.WriteLine(", stringVal = " + tok.getStringVal());
+                //else if (tt == TokenType.IDENT)
+                //  Console.WriteLine(", name = " + tok.getName());
+                //else
+                //  Console.WriteLine();
 
                 // Console.Write("Scheme 4101> ");
                 tok = scanner.getNextToken();
             }
             return 0;
         }
-
-        // Create parser
-        Parser parser = new Parser(scanner);
-        Node root;
-
-        // Parse and pretty-print each input expression
-        root = parser.parseExp();
-        while (root != null) 
-        {
-            root.print(0);
-            root = parser.parseExp();
-        }
-
         return 0;
     }
 }
